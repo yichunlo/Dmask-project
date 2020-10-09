52d64f6e8987f08eab201ddca0fd3c9cb25a928f
羅義鈞 <adjadj86@gmail.com>
2020-10-09 16:24:16 +0800

    Initial Commit

---
+++ Dmask_replace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dmask_replace
+//
+//  Created by 羅義鈞 on 2020/10/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dmask_replace/Dmask_replaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dmask_replaceApp.swift
+//  Dmask_replace
+//
+//  Created by 羅義鈞 on 2020/10/9.
+//
+
+import SwiftUI
+
+@main
+struct Dmask_replaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
